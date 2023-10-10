6ca8ac1e6c21f887d7ea0b916977dc6bd9a35da2
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-10-10 17:35:01 +0300

    ra-router-crud-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MainPage from './react-component/MainPage/MainPage';
+import AddPost from './react-component/AddPost/AddPost';
+import PostView from './react-component/PostView/PostView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="page">
+        <Switch>
+          <Route path="/posts/new" component={AddPost} />
+          <Route path="/posts/:id" component={PostView} />
+          <Route path="/" component={MainPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
